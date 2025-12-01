2b75dcd9e4a379f28990105f3802373637328677
jefedan <36.serrano.daniel.1cad@gmail.com>
2025-11-30 20:55:13 -0400

    Add LoginForm and update form fields
    Added a LoginForm for user authentication and updated fields in existing forms.

--- encuestas/forms.py
+++ encuestas/forms.py
@@ -1,17 +1,33 @@
 from django import forms
 from .models import Encuesta, Pregunta, Participante
 
+class LoginForm(forms.Form):
+    username = forms.CharField(label="Usuario")
+    password = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+
+
 class EncuestaForm(forms.ModelForm):
     class Meta:
         model = Encuesta
-        fields = ['titulo', 'descripcion']
+        fields = ['nombre', 'descripcion', 'activa']
+
 
 class PreguntaForm(forms.ModelForm):
     class Meta:
         model = Pregunta
-        fields = ['texto', 'tipo', 'opciones', 'orden']
+        fields = ['id_encuesta', 'texto']
+
 
 class ParticipanteForm(forms.ModelForm):
     class Meta:
         model = Participante
-        fields = ['nombre', 'email']
+        fields = ['nombre', 'correo', 'username', 'password', 'id_encuesta_asignada']
+
+
+class ResponderEncuestaForm(forms.Form):
+    OPCIONES = [
+        ('Regular', 'Regular'),
+        ('Bueno', 'Bueno'),
+        ('Excelente', 'Excelente'),
+        ('No Responde', 'No Responde'),
+    ]
